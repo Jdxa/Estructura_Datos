be4d52a86f09adfd3a2ef4251513bbb5b7f97236
Jdxa <dossantosjoaquin1@gmail.com>
2026-03-18 20:14:54 -0300

    Implementacion clase Nodo

---
+++ lineales.dinamicas/Nodo.java
@@ -0,0 +1,25 @@
+public class Nodo {
+    private Object dato;
+    private Nodo enlace;
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+        this.enlace = null;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(Nodo enlace) {
+        this.enlace = enlace;
+    }
+}
