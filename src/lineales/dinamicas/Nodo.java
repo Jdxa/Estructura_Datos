a4d776ce7aeed34825cb30bf69b4e970f8a77b7d
Jdxa <dossantosjoaquin1@gmail.com>
2026-03-26 12:53:57 -0300

    organizacion

---
+++ src/lineales/dinamicas/Nodo.java
@@ -0,0 +1,26 @@
+package lineales.dinamicas;
+class Nodo {
+    private Object dato;
+    private Nodo enlace;
+
+    public Nodo(Object dato, Nodo enlace) {
+        this.dato = dato;
+        this.enlace = enlace;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(Nodo enlace) {
+        this.enlace = enlace;
+    }
+}
---
+++ src/test/testPila.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import lineales.estaticas.*;
+
+public class testPila {
+    //palabra clave para el test
+    @Test
+    public void testearPila(){
+        
+   
+    }
+}
+
+
