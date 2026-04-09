04036417aaf5a34a2888ec6352bcbc9e2c40e61e
Jdxa <dossantosjoaquin1@gmail.com>
2026-04-09 19:57:55 -0300

    test

---
+++ src/test/lineales/testCola.java
@@ -0,0 +1,22 @@
+package test.lineales;
+public class testCola {
+    public static void main(String[] args) {
+        // Cola c1 = new Cola();
+        // Cola c2 = c1.clone();
+        // System.out.println(c1.poner(1));
+        // System.out.println(c1.poner(2));
+        // System.out.println(c1.poner(3));
+        // System.out.println(c1.obtenerFrente());
+        // c1.sacar();
+        // System.out.println(c1.obtenerFrente());
+        // c1.sacar();
+        // System.out.println(c1.obtenerFrente());
+        // c1.sacar();
+        // System.out.println(c1.obtenerFrente());
+        // System.out.println(c2.obtenerFrente());
+
+
+
+    }
+
+}
---
+++ src/test/lineales/testPila.java
@@ -0,0 +1,60 @@
+package test.lineales;
+public class testPila {    
+    public static void main(String[] args) {
+    //     //Pila p = new Pila();
+    //     // p.apilar(1);
+    //     // p.apilar(3);
+    //     // p.apilar(6);
+    //     // p.apilar(7);
+    //     // p.apilar(6);
+    //     // p.apilar(6);
+    //     // p.apilar(7);
+    //     // p.apilar(6);
+    //     // p.apilar(3);
+    //     // p.apilar(1);
+    //     // //clono p y creo p2
+    //     // Pila p2 = p.clone();
+    //     // imprimirPila(p);
+    //     // imprimirPila(p2);
+    //     // //pruebo que p y p2 no apunten a la misma direccion de memoria
+    //     // apuntan(p, p2);
+                
+        
+
+        
+
+
+
+
+    //     //System.out.println(Capicua(p));
+
+    // }
+    
+    
+    // public static void apuntan (Pila p1, Pila p2){
+    //     //evalua si estan en la misma direccion de memoria, para probar el clone
+    //     if (p1== p2) {
+    //         System.out.println("Las pilas apuntan a la misma direccion de memoria");
+    //     }else{
+    //         System.out.println("Las pilas no apuntan a la misma direccion de memoria");
+    //     }
+        
+
+        
+    // }
+    // public static void imprimirPila(Pila p){
+    //     Pila p2 = p.clone();
+    //     String pilaS= "";
+    //     while (!p2.esVacia()) {
+    //         pilaS = pilaS +p2.obtenerTope() + ", ";
+    //         p2.desapilar();
+    //     }
+    //     System.out.println(pilaS);
+        
+    // }
+
+}
+
+
+
+}
